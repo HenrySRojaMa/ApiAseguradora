0c34fc23f23cf2485764c1f0b50946f813694f76
HenrySRojaMa <henry.rojas.tb@gmail.com>
2024-10-26 20:47:46 -0500

    script de la base de datos

--- Data/SegurosContext.cs
+++ Data/SegurosContext.cs
@@ -25,8 +25,7 @@ namespace Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=dev.intecav.com,80;Database=Seguros;User ID=bvg_admin;Password=SuperSecret12345;Connection Timeout=30;MultipleActiveResultSets=False;Persist Security Info=False;");
+                optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("conexion"));
             }
         }
 
